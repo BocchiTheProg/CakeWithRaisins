c49464e5c71bc83574862ac16aa9ba27e946a733
BocchiTheProg <bocchitheprog@gmail.com>
2023-09-30 20:32:38 +0300

    Created separate method for cake validation.

--- CutTheCake.rb
+++ CutTheCake.rb
@@ -1,18 +1,20 @@
-cake = ".o.\n..o\n...\n.oo\nooo\no.o" #You can put your own example here
+def cake_valid? (cake)
+  false if cake.count("o") <=1 or cake.count("o") >= 10
+  array_cake = cake.split("\n")
+  cake_width = array_cake[0].size
+  array_cake.each do |i|
+    unless i.size == cake_width
+      return false
+    end
+  end
+  true
+end
+
+cake = ".o.\n..o\n..o" #You can put your own example here
 puts "cake =\n#{cake}"
 raisins = cake.count("o")
 puts "Amount of raisins:#{raisins}"
-array_cake = cake.split("\n")
-is_rectangle = true
-cake_height = array_cake.size
-cake_width = array_cake[0].size
-array_cake.each do |i|
-  unless i.size == cake_width
-    is_rectangle = false
-    break
-  end
-end
-if !is_rectangle or raisins <=1 or raisins >= 10
+unless cake_valid?(cake)
   puts "Sorry, your cake doesn't have rectangle form or amount of raisins is out of range (1;10)"
   exit
 end
