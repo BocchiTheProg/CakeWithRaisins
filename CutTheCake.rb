57236a5c4b9b8bf74699eca212734b6e5bfba222
BocchiTheProg <bocchitheprog@gmail.com>
2023-10-01 17:54:19 +0300

    Added comments to some methods and sections.

--- CutTheCake.rb
+++ CutTheCake.rb
@@ -1,3 +1,4 @@
+# Method for marking sliced pieces of cake
 def cut(ar_cake, x, y, width, height)
   (y...(y + height)).each do |i|
     (x...(x + width)).each { |j| ar_cake[i][j] = "*"}
@@ -5,6 +6,7 @@ def cut(ar_cake, x, y, width, height)
   ar_cake
 end
 
+# Transform array(which contains strings) into string form
 def arr_to_string(rectangle_form)
   result = ""
   rectangle_form.each do |i|
@@ -14,18 +16,22 @@ def arr_to_string(rectangle_form)
   result
 end
 
+# This method return hash with indexes of top left available spot(piece) in cake
 def find_first_not_cut_spot(ar_cake)
   (0...ar_cake.size).each do |i|
-    (0...ar_cake[i].size).each {|j| return {y:i, x:j} if ar_cake[i][j] != "*"}
+    (0...ar_cake[i].size).each { |j| return {y:i, x:j} if ar_cake[i][j] != "*"} # '*' in cake means this peaces is already cut
   end
   nil
 end
 
+# This method check if cutting some slice is possible
 def slice_valid(ar_cake, x, y, width, height)
-  return nil if (x + width) > ar_cake[0].size or (y + height) > ar_cake.size
-  slice = ar_cake.slice(y, height)
-  slice = slice.map { |i| i.slice(x, width)}
+  return nil if (x + width) > ar_cake[0].size or (y + height) > ar_cake.size # check if slice is not out of cake ranges(form)
+  # Literally `slicing` the cake array to get slice array
+  slice = ar_cake.slice(y, height) # rows
+  slice = slice.map { |i| i.slice(x, width)} # columns
   str_slice = arr_to_string(slice)
+  # You can uncomment code section below to see process of finding solution in console
   # puts "Trying to cut the slice:"
   # puts str_slice
   if str_slice.include? "*" or str_slice.count("o") != 1
@@ -35,13 +41,18 @@ def slice_valid(ar_cake, x, y, width, height)
   str_slice
 end
 
+# Recursive method which do all the main work
 def searching(ar_cake, slices, size)
+  # You can uncomment code section below to see process of finding solution in console
   # puts "Current slices:\n#{slices}"
   # puts "Current state(form) of cake:"
   # puts arr_to_string(ar_cake)
   position = find_first_not_cut_spot(ar_cake)
   # puts "Position of top left available spot:#{position}"
-  return slices if position == nil
+  return slices if position == nil # If its impossible to find that position, the solution already found (it is saved in slices array)
+  # According to the task requirements, wee need to cut the widest slice first,
+  # so we start with maximum possible slice width
+  # and minimum slice height
   slice_width = size
   loop do
     break if slice_width == 0
@@ -49,11 +60,15 @@ def searching(ar_cake, slices, size)
     loop do
       break if slice_height == size
       slice_height += 1
-      next unless slice_width * slice_height == size
+      next unless slice_width * slice_height == size # all slices must have equal sizes
       slice = slice_valid(ar_cake, position[:x], position[:y], slice_width, slice_height)
       next if slice == nil
-      new_slices = slices.dup
+      # Important, because we will be using recursion, even if some slices is valid, the other iterations may not lead to solution
+      # so, we must to COPY (CLONE) our slices array and cake array, and not just create a reference (like new_arr = arr)
+      # This way, it will be possible to track back and find other possible ways of cutting the cake
+      new_slices = slices.dup # copying the slice array
       new_slices.push(slice)
+      # Copying the 2d array cake (it's form)
       new_ar_cake = []
       i=0
       loop do
@@ -61,16 +76,20 @@ def searching(ar_cake, slices, size)
         new_ar_cake[i] = ar_cake[i].dup
         i += 1
       end
+      # It is possible to copy multi-dimensional arrays without loops (commented line below)
+      # new_ar_cake = Marshal.load(Marshal.dump(ar_cake)) more universal way
       new_ar_cake = cut(new_ar_cake, position[:x], position[:y], slice_width, slice_height)
-      res = searching(new_ar_cake, new_slices, size)
-      return res if res.size > 0
+      res = searching(new_ar_cake, new_slices, size) # using recursion
+      return res if res.size > 0 # returning slice array
     end
     slice_width -= 1
   end
+  # You can uncomment line below to see process of finding solution in console
   # puts "Impossible to continue cutting, returning back to previous state(form) of cake"
-  []
+  [] # returning empty array if solution wasn't found
 end
 
+# Checking if cake have the form if rectangle and the amount of raisins is correct
 def cake_valid?(cake)
   return false if cake.count("o") <=1 or cake.count("o") >= 10
   array_cake = cake.split("\n")
@@ -83,6 +102,7 @@ def cake_valid?(cake)
   true
 end
 
+# If user didn't write anything in input, program will take cake from code
 # You can put your own example here
 #cake = ".o.\n..o\n..o"
 #cake = "........\n..o.....\n...o....\n........"
@@ -102,6 +122,7 @@ unless cake_valid?(cake)
   exit
 end
 puts "Trying to cut the cake..."
+# We transform the cake from String to 2d Array because it will give us more convenient way to 'go through' cake
 array_cake = cake.split("\n")
 array_cake = array_cake.map { |i| i.split("")}
 slice_size = (array_cake.size * array_cake[0].size) / raisins
